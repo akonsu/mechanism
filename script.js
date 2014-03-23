15064be9924e0bf0ab58ae4f5487ff0a4f3d14a7
akonsu <akonsu@gmail.com>
2014-03-23 18:17:32 -0400

    fixed cursor and sticking of enlarged picture; draft

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //
 // fix window.requestAnimationFrame
 //
+
+//change test
+
 if (!window.requestAnimationFrame) {
     window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
                                     window.mozRequestAnimationFrame ||
@@ -37,17 +40,29 @@ if (!Array.prototype.filter) {
 (function (window) {
     var auto_rotate = true;
     var click;
-    var container;
+    var mechAniCalContainer;
+    var mechAniCalCursorContainer;
     var dragging = false;
     var dx;
     var dy;
     var forwards;
     var frame_num = 0;
     var frames = [];
+    var mouseover_counter;
     var rotating = false;
     var x_offset;
     var y_offset;
     var x_prev;
+
+    var UPDATE_INTERVAL = 1000 / 5; // initial rotation speed in milliseconds per frame
+    var UPDATE_INTERVAL_DRAG = 1000 / 20; // rotation speed after clicking the image in milliseconds per frame
+
+
+    // Inline cursor styles: 
+    var cursorDrag=' url("cursorImages/cuDrag.cur"), auto';
+    var cursorReduce=' url("cursorImages/cuReduce.cur"), auto';
+    var cursorOver=' url("cursorImages/cuOver.cur"), auto';
+    var cursorZoomDrag=' url("cursorImages/cuZoomDrag.cur"), auto';
 
     var loadOrder = [
         "shoeImages/A.jpg",
@@ -75,7 +90,7 @@ if (!Array.prototype.filter) {
 
     function animate(prev_time) {
         if (rotating){
-            var UPDATE_INTERVAL = 1000 / 20; // rotation speed in milliseconds per frame
+
             var time = +new Date();          // same as new Date().getTime()
 
             if (time > prev_time + UPDATE_INTERVAL) {
@@ -113,17 +128,19 @@ if (!Array.prototype.filter) {
         return function () { f.apply(window, _arguments) };
     }
 
-    function event_bind(element, name, handler) {
+    function event_bind(element, name, handler,capture) {
+        //capture=true for capture phase listener, capture= false for bubble phase listener
         if (element.addEventListener) {
-            element.addEventListener(name, handler, false);
+            element.addEventListener(name, handler, capture);
         } else if (element.attachEvent) {
             element.attachEvent("on" + name, handler);
         }
     }
 
-    function event_unbind(element, name, handler) {
+    function event_unbind(element, name, handler,capture) {
+        //capture=true for capture phase listener, capture= false for bubble phase listener
         if (element.removeEventListener) {
-            element.removeEventListener(name, handler, false);
+            element.removeEventListener(name, handler, capture);
         } else if (element.detachEvent) {
             element.detachEvent("on" + name, handler);
         }
@@ -149,7 +166,7 @@ if (!Array.prototype.filter) {
                 var count = frames.length;
                 var path = this.src;
 
-                // BEGIN--REMOVE FROM PRODUCTION CODE
+                // BEGIN--REMOVE FROM PRODUCTION CODE -- download indicator
                 try {
                     var ul = document.getElementById("downloadIndicator");
                     var li = document.createElement("li");
@@ -172,7 +189,7 @@ if (!Array.prototype.filter) {
 
                 // insert element
                 this.style.display = this === frames[frame_num] ? "" : "none";
-                container.appendChild(this);
+                mechAniCalContainer.appendChild(this);
 
                 // load next frame
                 load_frame(index + 1);
@@ -187,6 +204,10 @@ if (!Array.prototype.filter) {
     }
 
     function rotate_onmousedown(e) {
+        event_bind(document,"mousemove",rotate_onmousemove,false);
+        event_bind(document, "touchmove",rotate_ontouchmove,false);
+        // event_bind(mechAniCalCursorContainer,"mousemove",rotate_onmousemove,false);
+        // event_bind(mechAniCalCursorContainer, "touchmove",rotate_ontouchmove,false);
         if (!dragging) {
             var v = e || window.event;
 
@@ -194,6 +215,7 @@ if (!Array.prototype.filter) {
             click = true;
             dragging = true;
             rotating = false;
+            UPDATE_INTERVAL = UPDATE_INTERVAL_DRAG; // Increase rotation speed for dragging
 
             x_offset = v.offsetX;
             y_offset = v.offsetY;
@@ -203,16 +225,19 @@ if (!Array.prototype.filter) {
     }
 
     function rotate_onmousemove(e) {
+        if (e.preventDefault) e.preventDefault();
+
         if (dragging && frames.length > 0) {
             var v = e || window.event;
 
-            var DELTA = 3; // minimal number of pixels that the mouse (a finger) has to move
+            var DELTA = 3; // minimal number of pixels that the mouse (finger) has to move
             var d = x_prev - v.screenX;
 
             if (Math.abs(d) > DELTA) {
                 if (click) {
-                    css_class_add(document.body, "cursor-drag");
-                    css_class_remove(container, "cursor-over");
+
+                    mechAniCalCursorContainer.style.cursor=cursorDrag; 
+                    document.body.style.cursor=cursorDrag; 
                     click = false;
                 }
                 forwards = d < 0
@@ -224,6 +249,11 @@ if (!Array.prototype.filter) {
     }
 
     function rotate_onmouseup() {
+        event_unbind(document,"mousemove",rotate_onmousemove,false);
+        event_unbind(document,"touchmove",rotate_ontouchmove,false);
+        // event_unbind(mechAniCalCursorContainer,"mousemove",rotate_onmousemove,false);
+        // event_unbind(mechAniCalCursorContainer,"touchmove",rotate_ontouchmove,false);
+
         if (dragging) {
             if (click) {
                 var frame = frames[frame_num];
@@ -237,13 +267,13 @@ if (!Array.prototype.filter) {
                 frame.client_width = r.width;
                 frame.client_height = r.height;
 
-                var dw = container.client_width - r.width;
-                var dh = container.client_height - r.height;
+                var dw = mechAniCalContainer.client_width - r.width;//mechAniCalContainer width minus big pic. width=left coord. of big pic.
+                var dh = mechAniCalContainer.client_height - r.height;
 
-                var dx = (x_offset - parseInt(frame.style.left + 0) - r_orig.width / 2);
+                var dx = (x_offset - parseInt(frame.style.left + 0) - r_orig.width / 2); //old distance from cursor to center
                 var dy = (y_offset - parseInt(frame.style.top + 0) - r_orig.height / 2);
 
-                var cx = dw / 2 - dx * r.width / r_orig.width;
+                var cx = dw / 2 - dx * r.width / r_orig.width;//left coord. of big pic.-cursor coord. in big pic.
                 var cy = dh / 2 - dy * r.height / r_orig.height;
 
                 var x = Math.min(0, Math.max(dw, cx));
@@ -252,23 +282,22 @@ if (!Array.prototype.filter) {
                 frame.style.left = x + "px";
                 frame.style.top = y + "px";
 
-                container.onmousedown = zoom_onmousedown;
-                document.onmousemove = zoom_onmousemove;
-                document.onmouseup = zoom_onmouseup;
+                event_unbind(mechAniCalCursorContainer, "mousedown", rotate_onmousedown,false);
+                event_unbind(document, "mouseup", rotate_onmouseup,false);
 
-                event_unbind(container, "touchstart", rotate_ontouchstart);
-                event_unbind(container, "touchmove", rotate_ontouchmove);
-                event_unbind(container, "touchend", rotate_ontouchend);
+                event_bind(mechAniCalCursorContainer, "mousedown", zoom_onmousedown,false);
+                event_bind(document, "mouseup", zoom_onmouseup,false);
 
-                event_bind(container, "touchstart", zoom_ontouchstart);
-                event_bind(container, "touchmove", zoom_ontouchmove);
-                event_bind(container, "touchend", zoom_ontouchend);
+                event_unbind(mechAniCalCursorContainer, "touchstart", rotate_ontouchstart,false);
+                event_unbind(document, "touchend", rotate_ontouchend,false);
 
-                css_class_add(container, "cursor-reduce");
-                css_class_remove(container, "cursor-over");
+                event_bind(mechAniCalCursorContainer, "touchstart", zoom_ontouchstart,false);
+                event_bind(document, "touchend", zoom_ontouchend,false);
+
+                mechAniCalCursorContainer.style.cursor=cursorReduce; 
             } else {
-                css_class_add(container, "cursor-over");
-                css_class_remove(document.body, "cursor-drag");
+                mechAniCalCursorContainer.style.cursor=cursorOver; 
+                document.body.style.cursor='';
             }
             dragging = false;
             rotating = false;
@@ -292,7 +321,14 @@ if (!Array.prototype.filter) {
     }
 
     function zoom_onmousedown(e) {
+        mouseover_counter=0;
+        event_bind(document,"mousemove",zoom_onmousemove,false);
+        event_bind(document,"touchmove",zoom_ontouchmove,false);
+        // event_bind(mechAniCalCursorContainer,"mousemove",zoom_onmousemove,false);
+        // event_bind(mechAniCalCursorContainer,"touchmove",zoom_ontouchmove,false);
+ 
         if (!dragging) {
+
             var v = e || window.event;
             var frame = frames[frame_num];
 
@@ -305,18 +341,23 @@ if (!Array.prototype.filter) {
     }
 
     function zoom_onmousemove(e) {
-        if (dragging) {
+        if (e.preventDefault) e.preventDefault();
+        mouseover_counter=mouseover_counter+1; //counts mousemove events
+        var mouseover_counter_rem=mouseover_counter%5; 
+
+        if (dragging & mouseover_counter_rem===0) { //following code executes on every fifth mousmove event
+
             var v = e || window.event;
             var frame = frames[frame_num];
-            var x = Math.min(0, Math.max(container.client_width - frame.client_width, dx + v.clientX));
-            var y = Math.min(0, Math.max(container.client_height - frame.client_height, dy + v.clientY));
+            var x = Math.min(0, Math.max(mechAniCalContainer.client_width - frame.client_width, dx + v.clientX));
+            var y = Math.min(0, Math.max(mechAniCalContainer.client_height - frame.client_height, dy + v.clientY));
 
             frame.style.left = x + "px";
             frame.style.top = y + "px";
 
             if (click) {
-                css_class_add(document.body, "cursor-zoom-drag");
-                css_class_remove(container, "cursor-reduce");
+                mechAniCalCursorContainer.style.cursor=cursorZoomDrag; 
+                document.body.style.cursor=cursorZoomDrag; 
                 click = false;
             }
             return false;
@@ -324,33 +365,46 @@ if (!Array.prototype.filter) {
     }
 
     function zoom_onmouseup() {
+        event_unbind(document,"mousemove",zoom_onmousemove,false);
+        event_unbind(document,"touchmove",zoom_ontouchmove,false);
+        // event_unbind(mechAniCalCursorContainer,"mousemove",zoom_onmousemove,false);
+        // event_unbind(mechAniCalCursorContainer,"touchmove",zoom_ontouchmove,false);
+
         if (dragging) {
+
             if (click) {
+
                 var frame = frames[frame_num];
 
-                frame.style.left = "";
-                frame.style.top = "";
                 frame.style.width = "";
                 frame.style.height = "";
+                frame.style.left = "";
+                frame.style.top = "";
 
-                container.onmousedown = rotate_onmousedown;
-                document.onmousemove = rotate_onmousemove;
-                document.onmouseup = rotate_onmouseup;
+                event_unbind(mechAniCalCursorContainer, "mousedown", zoom_onmousedown,false);
+                event_unbind(document, "mouseup", zoom_onmouseup,false);
 
-                event_unbind(container, "touchstart", zoom_ontouchstart);
-                event_unbind(container, "touchmove", zoom_ontouchmove);
-                event_unbind(container, "touchend", zoom_ontouchend);
+                event_bind(mechAniCalCursorContainer, "mousedown", rotate_onmousedown,false);
+                event_bind(document, "mouseup", rotate_onmouseup,false);
 
-                event_bind(container, "touchstart", rotate_ontouchstart);
-                event_bind(container, "touchmove", rotate_ontouchmove);
-                event_bind(container, "touchend", rotate_ontouchend);
+                event_unbind(mechAniCalCursorContainer, "touchstart", zoom_ontouchstart,false);
+                event_unbind(document, "touchend", zoom_ontouchend,false);
 
-                css_class_add(container, "cursor-over");
-                css_class_remove(container, "cursor-reduce");
+                event_bind(mechAniCalCursorContainer, "touchstart", rotate_ontouchstart,false);
+                event_bind(document, "touchend", rotate_ontouchend,false);
+
+                mechAniCalCursorContainer.style.cursor=cursorOver; 
+
+        //mechAniCalContainer.style.cursor=cursorOver; //new addition
+        // frame.style.cursor=cursorOver; //new addition
+        // mechAniCalContainer.img.style.cursor=cursorOver; //new addition
+        // document.getElementById("mechAncileryImages").style.cursor=cursorOver;//new addition
+        // document.body.style.cursor=cursorOver;//new addition
+
             }
             else {
-                css_class_add(container, "cursor-reduce");
-                css_class_remove(document.body, "cursor-zoom-drag");
+                mechAniCalCursorContainer.style.cursor=cursorReduce; 
+                document.body.style.cursor=''; 
             }
             dragging = false;
         }
@@ -373,30 +427,40 @@ if (!Array.prototype.filter) {
     }
 
     window.onload = function () {
-        container = document.getElementById("container");
 
-        var r = get_bounding_rect(container);
+        mechAniCalContainer = document.getElementById("mechAnimationContainer");
+        mechAniCalCursorContainer = document.getElementById("mechBoundingBox"); // cursor is attached to the parent div of the picture
 
-        container.client_width = r.width;
-        container.client_height = r.height;
+        var r = get_bounding_rect(mechAniCalContainer);
 
-        container.onmousedown = rotate_onmousedown;
-        document.onmousemove = rotate_onmousemove;
-        document.onmouseup = rotate_onmouseup;
+        mechAniCalContainer.client_width = r.width;
+        mechAniCalContainer.client_height = r.height;
 
-        event_bind(container, "touchstart", rotate_ontouchstart);
-        event_bind(container, "touchmove", rotate_ontouchmove);
-        event_bind(container, "touchend", rotate_ontouchend);
+        event_bind(mechAniCalCursorContainer, "mousedown", rotate_onmousedown,false);
+        event_bind(document, "mouseup", rotate_onmouseup,false);
+
+        event_bind(mechAniCalCursorContainer, "touchstart", rotate_ontouchstart,false);
+        event_bind(document, "touchend", rotate_ontouchend,false);
 
         // remove existing elements
-        while (container.hasChildNodes()) {
-            container.removeChild(container.firstChild);
+        while (mechAniCalContainer.hasChildNodes()) {
+            mechAniCalContainer.removeChild(mechAniCalContainer.firstChild);
         }
+
+        cursor_box = document.getElementById("cursorBox");
+
         load_frame(0);
 
-        css_class_add(container, "cursor-over");
+        mechAniCalCursorContainer.style.cursor=cursorOver;
+        //??document.body.style.cursor=cursorOver; 
+try {
+    document.execCommand('BackgroundImageCache', false, true);
+} 
+catch (e) {}
 
         // start animation loop
-        animate(+new Date());
+        window.requestAnimationFrame(delay(animate, +new Date()));        
+        //animate(+new Date());
     };
+
 })(window);
